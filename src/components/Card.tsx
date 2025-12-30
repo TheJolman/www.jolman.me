642a2ceea9b9d886db9f937913a16b6ad7b11323
Josh Holman <git@jolman.me>
2025-12-29 19:32:10 -0800

    conditionally render title

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,12 @@
-export default function Card({ title, children }) {
+export default function Card({ title = "", children }) {
   return (
     <div className="card bg-base-200 shadow-200 shadow-xl border border-base-300">
       <div className="card-body">
-        <div className="card-title">
-          {title}
-        </div>
+        {title && (
+          <div className="card-title">
+            {title}
+          </div>
+        )}
         {children}
       </div>
     </div>
