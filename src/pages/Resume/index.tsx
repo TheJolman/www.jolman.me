b149a1bc7c82d062fa64a9e68aecbfe1a46d2290
Josh Holman <git@jolman.me>
2025-12-29 19:32:21 -0800

    simplify resume render + style

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -1,3 +1,4 @@
+import Card from '../../components/Card'
 import { useState, useEffect } from 'preact/hooks';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
@@ -12,6 +13,10 @@ export function Resume() {
       .then((text) => setMarkdown(text));
   }, []);
 
+  if (!markdown) {
+    return <p>Fetching my most recent resume...</p>
+  }
+
   return (
     <div class="max-w-3xl mx-auto flex flex-col items-center mb-6">
       <div className="flex justify-center my-4">
@@ -24,8 +29,10 @@ export function Resume() {
           Download PDF
         </a>
       </div>
-      <main className="resume-container p-4 bg-base-200 rounded-box shadow-lg">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+      <main className="resume-container">
+        <Card>
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+        </Card>
       </main>
     </div>
   );
