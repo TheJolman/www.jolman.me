83a340d0989803534f03d5f8606c540ce7a4e6d5
Josh Holman <git@jolman.me>
2025-12-30 11:35:30 -0800

    add title to portfolio

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -49,7 +49,11 @@ export function Portfolio() {
 
   return (
     <div className="max-w-3xl mx-auto flex flex-col mb-6">
-      <div className="flex justify-center my-4 gap-4">
+      <div className="p-4 text-center">
+        <h1 className="text-2xl font-bold mb-6">Professional Portfolio</h1>
+      </div>
+
+      <div className="flex justify-center mb-4 gap-4">
         {links.map((link) => (
           <a href={link.href} key={link.href} className="btn btn-neutral">
             {link.content}
