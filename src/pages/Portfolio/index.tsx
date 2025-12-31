aec2e13f094c24c86e31e38cdc14671d23a8206c
Josh Holman <git@jolman.me>
2025-12-30 23:29:15 -0800

    update portfolio content

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -71,10 +71,32 @@ export function Portfolio() {
             acm@CSUF's official data layer. Utilized Discord OAuth2 to allow club members with sufficient roles
             in the main discord server to modify or view resources such as events, members, and announcements.
             Repository contains both API server as well a lightweight CLI client useful for querying the
-            server and testing the OAuth2 flow. My role was lead developer - I did and still do plan/create
+            server and testing the OAuth2 flow.
+            <br />
+            My role was lead developer: I did and still do plan/create
             the largest changes the project requires. I also delegated tasks to our other developers and
             worked through blockers as they came up. I created our current dev shell and CI pipeline that
             enables supreme reproducability and code quality standards on merge.
+            <br />
+            Our dev environment is based on Nix + direnv for a declarative and isolated dev shell
+            that's lighter than Docker and loads/unloads automatically. Also included is a Nix
+            package definition that allows anyone with nix installed to run our app(s) with
+            <code className="text-accent"> nix run </code>or by adding our repo to their flake
+            inputs. Our CI pipeline of course takes advantage of this as well, ensuring that
+            the exact environment that our developers are using is being used in CI as well.
+            <br />
+            Due to the nature of our constantly changing team of students, I've made documentation
+            a core tenent of this project. When it's not immediately obvious why something exists,
+            we write docs. Also included are docs that help new developers get started, especially
+            since it's a rapidly growing codebase with tools they may have never used before. This
+            includes details on using our pre-written fixtures for making test HTTP requests,
+            explaining the project structure and where new code might belong, and explaining
+            the various systems we take advantage of like our Disocrd OAuth2 middleware layer or
+            OpenAPI 2 (Swagger) docs. Speaking of Swagger, this was one of the things we prioritized
+            early. It made testing the API and understanding what was there much easier for the
+            team. You might be wondering why we're not using OpenAPI 3 docs, and it's becuase
+            we're writing our service with Gin. We hope to move to OpenAPI 3 docs at some point
+            in the future, but it's currently low priority as Swagger is doing its job pretty well.
           </p>
         </ProjectCard>
 
@@ -86,6 +108,13 @@ export function Portfolio() {
           <p>
             Full-stack web application that rates playlists, albums and songs using popularity data from
             Spotify. Created CI/CD pipeline to enforce code quality and ensure production is up to date.
+            <br />
+            This was the first collaborative, full-stack web app I've worked on. We were a team of
+            three, and while the app wasn't huge, the learning experience was. I had to learn
+            Django for this project (which I liked quite a bit) and back-end web dev in general.
+            <br />
+            This app was originally deployed to PythonAnywhere by Anaconda, but later I containerized
+            the app and migrated it to fly.io for its pricing model.
           </p>
         </ProjectCard>
 
@@ -94,7 +123,23 @@ export function Portfolio() {
           technologies="Go, Docker, Terraform, Postgres, MinIO, React, Tanstack Router"
           sourceLink="https://github.com/tomasohchom/motion"
         >
-          <p>asdf</p>
+          <p>
+            This was a large full stack, microservice architecture web app written for our senior project.
+            I worked exclusively on our Services written with Go + Postgres (sqlc), and containerized with Docker.
+            Architecting this project was truly a blast, I learned so much about writing independent
+            services that can work together.
+            <br />
+            The service I spent the most time with was a file upload service intended to be used with
+            AWS S3, but ended up using MinIO instead for minimizing cost during development.
+            Docker compose proved extremely useful for this service, as we had not only an addional
+            MinIO container running with our image, but also a Postgres metadata store. We used this
+            since querying a database with a persistent connection is much much more performant
+            than sending comparatively slow HTTP requests to MinIO every time you want an update
+            on its content.
+            <br />
+            I wrote comprehensive Terraform IaC for deploying this entire project to AWS, but
+            the app is not currently live for cost reasons.
+          </p>
         </ProjectCard>
 
         <ProjectCard
@@ -118,7 +163,8 @@ export function Portfolio() {
         >
           <p>
             Terraform Infrastructure as Code (IaC) for deploying MediaWiki on AWS with proper configuration
-            and security.
+            and security. Highly customizable, intended to be easy to use for anyone who wants
+            to quickly deploy a Mediawiki instance on AWS.
           </p>
         </ProjectCard>
 
@@ -129,7 +175,10 @@ export function Portfolio() {
         >
           <p>
             Simple and fast CLI that keeps track of priorities. Uses file serialization and shortest string
-            matching for easy use.
+            matching for easy use. Written with bleeding-edge C++23 since it's a personal project.
+            Hello <code className="text-accent">std::print</code> and <code className="text-accent">std::optional</code>!
+            <br />
+            Uses pre-commit hooks for maintainting code quality that I'm super picky about.
           </p>
         </ProjectCard>
       </div>
