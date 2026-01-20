9bb27676a223ab654a14a4e3f12d85345dbdd6cf
Josh Holman <git@jolman.me>
2026-01-19 23:01:05 -0800

    redesign homepage (#29)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,45 @@
-import { PageLinks } from "../../constants/buttons";
+import asciiArt from '../../assets/cat_box.txt';
+import { useState, useEffect } from 'preact/hooks';
+import './style.css';
+
+const AsciiLoader = () => {
+  const [art, setArt] = useState('');
+
+  useEffect(() => {
+    fetch(asciiArt)
+      .then((response) => response.text())
+      .then((data) => setArt(data))
+      .catch((error) => console.error('Could not fetch ASCII art:', error));
+  });
+
+  return (
+    <pre><code>{art}</code></pre>
+  )
+}
 
 export function Home() {
   return (
-    <>
-      <head>
-        <title>Josh Holman - CS Student & Math Tutor</title>
-      </head>
-      <div className="hero min-h-[calc(100vh-10rem)]">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              I'm Josh Holman, a software developer
-              and tutor. Welcome to my site.
-            </p>
-            <div className="flex flex-col items-center join join-vertical">
-              {PageLinks.map((button) => (
-                <a
-                  href={button.href}
-                  className="btn btn-primary btn-wide join-item shadow-xl border-base-300 border"
-                  key={button.href}
-                >
-                  {button.content}
-                </a>
-              ))}
-            </div>
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row w-full px-4 lg:px-8">
+        <div className="mockup-code shadow-2xl lg:w-auto overflow-x-auto">
+          <pre data-prefix="❯"><span className="text-primary">nix run</span> github:thejolman/art</pre>
+          <br />
+          <div className="px-4 sm:px-10 overflow-x-auto">
+            <AsciiLoader />
           </div>
+          <pre data-prefix="❯" className="animate-blink"></pre>
+        </div>
+        <div className="text-center max-w-xl">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">Welcome to my site</h1>
+          <p className="py-6 text-sm sm:text-base">
+            I'm <b>Josh Holman</b>, a software developer and tutor.
+            <br />
+            I'm also an Open-source contributor and recent grad from CSU, Fullerton.
+            <br />
+            Currently looking for work in back-end SWE and DevOps.
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
