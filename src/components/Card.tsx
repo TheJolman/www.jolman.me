0cf0e09eda57ab83888cb853e6b1c2836dae9028
Josh Holman <git@jolman.me>
2026-04-07 10:51:19 -0700

    edit style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export default function Card({ title, children }: CardProps) {
   return (
-    <div className="card bg-base-200 shadow-200 shadow-xl border border-base-300">
+    <div className="card bg-base-200 shadow-xl border border-base-300">
       <div className="card-body">
         {title && (
           <div className="card-title">
